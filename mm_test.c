7d597bf05cc10bbc02ad7e180746a2c01a636270
abdulkadirdere <752817@students.wits.ac.za>
2015-10-05 22:47:36 +0200

    trying

--- mm_test.c
+++ mm_test.c
@@ -10,8 +10,8 @@ int main(int argc, char **argv)
 {
     int *data;
   
-    /* Variation 1 */
-    /* Simple test: invoke mm_malloc to allocate 4 byes and use mm_free to free it */
+    /* Test 1 */
+    /* Simple test: invoke mm_malloc to allocate 4 bytes and use mm_free to free it */
     data = (int*) mm_malloc(4);
    
     printf("_main @ %lx\n", main);
@@ -21,23 +21,21 @@ int main(int argc, char **argv)
 
     data[0] = 1;
     mm_free(data);
-    printf("VAR1: malloc sanity test successful!\n");
+    printf("Test1: malloc sanity test successful!\n");
 
-    /* Variation 2 */
-    /* Allocate zero byte: invoke mm_malloc to allocate 0 byes and use mm_free to free it */
+    /* Test 2 */
+    /* Allocate zero byte: invoke mm_malloc to allocate 0 bytes and use mm_free to free it */
     data = (int*) mm_malloc(0);
-    /* data[0] = 0; */
     mm_free(data);
-    printf("VAR2: zero test successful!\n");
+    printf("Test2: zero test successful!\n");
 
-    /* Variation 3 */
+    /* Test 3 */
     /* Allocate zero byte: invoke mm_malloc to allocate 0 byes and use mm_free to free it */
     data = (int*) mm_malloc(-1);
-    /* data[0] = 0; */
     mm_free(data);
-    printf("VAR3: negative test successful!\n");
+    printf("Test3: negative test successful!\n");
 
-    /* Variation 4 */
+    /* Test 4 */
     /* Verify the first fitting algorithm works correctly */
     void* array[10];
     array[0] = (int*)mm_malloc(5);
@@ -45,7 +43,6 @@ int main(int argc, char **argv)
     array[2] = (int*)mm_malloc(4);
     array[3] = (int*)mm_malloc(15);
     array[4] = (int*)mm_malloc(10);
-    /* data[0] = 0; */
     mm_free(array[3]);
     array[5] = (int*)mm_malloc(10);
     printf("array[3] %x, array[5] %x\n", array[3], array[5]);
@@ -56,21 +53,21 @@ int main(int argc, char **argv)
 	
     assert(array[3] == array[5]);
     mm_free(array[5]);
-    printf("VAR4: OK, the first fitting algorithm works correctly!\n");
+    printf("Test 4:the first fitting algorithm works correctly!\n");
 	
     array[1] = (int*)mm_malloc(5);
     fprintf(stderr, "array[0]: %x array[1]: %x\n", array[0], array[1]);
     assert(array[0] == array[1]);
-    fprintf(stderr, "VAR5: OK\n");
     mm_free(array[1]);
-    printf("VAR5: OK, the first fitting algorithm works correctly!\n");
+    printf("Test 5:the first fitting algorithm works correctly!\n");
 
     array[1] = (int*)mm_malloc(20);
-    fprintf(stderr, "array[0]: %x array[1]: %x\n", array[0], array[1]);
+    array[4] = (int*)mm_malloc(5);
+    fprintf(stderr, "array[1]: %x array[4]: %x\n", array[1], array[4]);
     assert(array[1] > array[4]);
-    fprintf(stderr, "VAR6: OK\n");
     mm_free(array[1]);
-    printf("VAR6: OK, the first fitting algorithm works correctly!\n");
+    mm_free(array[4]);
+    printf("Test 6: the first fitting algorithm works correctly!\n");
 
     array[0] = (int*)mm_malloc(5);
     array[1] = (int*)mm_malloc(10);
@@ -88,7 +85,7 @@ int main(int argc, char **argv)
     assert(array[0] == array[3]);
     mm_free(array[0]);
     mm_free(array[2]);
-    printf("VAR7: OK, the fusion function works correctly!\n");
+    printf("Test 7: the fusion function works correctly!\n");
      
     return 0;
 }
